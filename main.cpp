15c538c5c6cd1b0326a083739081cb1ad8955df0
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-11-10 23:04:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
 
    if(n>=2){
        cout<<"1"<<endl;
-       for(int i=1;i<=n-1;i++){
+       for(int i=2;i<=n;i++){
            c= a+b;
            a=b, b=c;
            cout<< c << endl;
