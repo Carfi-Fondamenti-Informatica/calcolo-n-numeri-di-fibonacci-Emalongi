aaec5b924d2d8b397134637e1ededba7fdf92414
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-11-10 21:32:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <iostream> // 1,1,2,3,5,8,13 a+b=c a=b b=c
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n=0,i=0, a=i,b=1, c=0;
+            cin>>n;
+
+   if(n>=2){
+       cout<<"1"<<endl;
+       for(int i=2;i<=n;i++){
+           c= a+b;
+           a=b, b=c;
+           cout<< c << endl;
+       }
+   }else{
+       cout<<"errore";
+   }
+
+  return 0;
 }
